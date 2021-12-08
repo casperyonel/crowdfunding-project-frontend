f203982fbc2072fe93f5c15d41cfe0b072b0378e
Casper Yonel <yonelcasper@gmail.com>
2021-12-08 15:58:52 -0700

    app built out mvp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import { Router } from 'react-router';
+import { Menu } from 'semantic-ui-react';
+import e from 'express';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <Router history={history}>
+       <Container>
+         <Menu secondary>
+           <Menu.Item 
+           name='home'
+           onClick={this.navigateToHome}
+           />
+         </Menu>
+
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/campaigns/:address" component={Campaign} />
+          <Route component={NotFound} />
+        </Switch>
+
+       </Container>
+     </Router>
   );
+
+  navigateToHome(e) {
+    e.preventDefault()
+    history.pushState('/')
+  }
 }
 
 export default App;
---
+++ src/components/NotFound.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h1>Oops! Page not found. </h1>            
+        </div>
+    )
+}
+
+export default NotFound
